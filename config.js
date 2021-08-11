e859173a21beeb5692c79626949d9f358cca2bba
maicarem123 <88781505+maicarem123@users.noreply.github.com>
2021-08-11 20:02:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Alo Nhật ớiiii!',
+    introDesc: `Hôm nay chỗ em vừa nắng vừa mưa nên có cầu vồng đẹp lắm nhưng mà anh có biết là vừa anh vừa em thì sẽ có một tình yêu đẹp đó =))))`,
+    btnIntro: 'Anh ơi bấm vào!',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên?',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy?',
+    btnYes: 'Thích em từ đầu rồi',
+    btnNo: 'Không, đâu dễ vậy',
+    question:'Tại sao anh lại thích em?',
+    btnReply: 'Trả lời đi =)))) Anh không có sự lựa chọn =))',
+    reply: 'Vì em dễ thương đóooooo',
+    mess: 'Em nhận thức được điều đó mà nhưng mà anh có thể ib em lý do nha *cry* vì em không biết chỉnh code này :)',
+    messDesc: 'Tối mai gọi điện khum anhhhhhh. Anh lại không có sự lựa chọn đâu :)',
+    btnAccept: 'Okeeeeeeeee.',
+    messLink: 'https://www.facebook.com/messages/t/maicarem'
+}
